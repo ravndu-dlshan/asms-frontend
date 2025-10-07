e86f7d6ea56dbadd8db7e45d6ad9f6e03167cec8
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-10-08 00:03:24 +0530

    create customer dashboard page

---
+++ src/app/customer/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import CustomerDashboardHero from './components/heroSection';
+
+const CustomerDashBoard = () => {
+    return (
+        <>
+            <Navbar />
+            <CustomerDashboardHero />
+        </>
+    );
+};
+
+export default CustomerDashBoard;
