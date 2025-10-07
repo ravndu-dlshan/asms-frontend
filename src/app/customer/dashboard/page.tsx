ca53a810d2d29eba1455b6331c3a33a145bcfeb0
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-10-08 00:47:41 +0530

    modify: customer dashboard page

--- src/app/customer/dashboard/page.tsx
+++ src/app/customer/dashboard/page.tsx
@@ -1,12 +1,21 @@
+'use client';
 import React from 'react';
-import Navbar from './components/navbar';
+import Navbar from '../components/navbar';
 import CustomerDashboardHero from './components/heroSection';
+import CusDashboardStat from './components/cusStat';
+import ServiceSection from './components/serviceCard';
+import FooterSection from '../components/footer';
+import ContactSection from './components/contactSection';
 
 const CustomerDashBoard = () => {
     return (
         <>
             <Navbar />
             <CustomerDashboardHero />
+            <ServiceSection />
+            <CusDashboardStat />
+            <ContactSection />
+            <FooterSection />
         </>
     );
 };
