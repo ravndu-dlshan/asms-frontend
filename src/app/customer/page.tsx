304b4b1716beb796d397740bd73c8da7b4b8cdbe
ShehanRajapaksha <shehan.rajapaksha2002@gmail.com>
2025-11-02 16:17:42 +0530

    bug fixes in merge

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -3,11 +3,12 @@
 import React from 'react'
 import Dashboard from './dashboard/page';
 
+export default function page() {
   return (
     <div>
       {/* <h1>{localStorage.getItem("userInfo")}</h1> */}
       <Dashboard />
       {/* <h1>Hi this is Customer Here!</h1> */}
     </div>
-  );
+  )
 }
